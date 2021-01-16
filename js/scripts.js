2306b7b13618533ab3ef52cee6183864495896c1
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-15 18:11:48 -0600

    add js scroll branching skeleton

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
   $("#quiz").submit(function(event) {
-  event.preventDefault();
-  const trustChoice = $("input:radio[name=trust]:checked").val();
-  console.log(trustChoice);
-  const seasonChoice = $("#season").val();
-  const coinAmount = parseInt($("#coins").val());
-  const distance = Math.floor(parseInt($("#mcdonalds").val()));
-  const genre = $("#music").val();
+    event.preventDefault();
+    const trustChoice = $("input:radio[name=trust]:checked").val();
+    console.log(trustChoice);
+    const seasonChoice = $("#season").val();
+    const coinAmount = parseInt($("#coins").val());
+    const distance = Math.floor(parseInt($("#mcdonalds").val()));
+    const genre = $("#music").val();
 
-  if (trustChoice === "tiger" && seasonChoice === "Autumn" && genre === "hip-hop" || coinAmount > 100 && distance <= 5) {
-    $("#c-sharp").show();
-  } else if (trustChoice === "carole" && seasonChoice === "Summer" || coinAmount <= 10 && genre === "classical") {
-    $("#python").show();
-  } else {
-    $("#swift").show();
-  }
+    if (trustChoice === "tiger" && seasonChoice === "Autumn" && genre === "hip-hop" || coinAmount > 100 && distance <= 5) {
+      $("#c-sharp").show();
+    } else if (trustChoice === "carole" && seasonChoice === "Summer" || coinAmount <= 10 && genre === "classical") {
+      $("#python").show();
+    } else {
+      $("#swift").show();
+    }
+    if ("button").click(event) {
+      event.submit()
+      $("html, body").animate({scrollBottom: $("button").offset().top}, 2000);
+    };
   });
 });
